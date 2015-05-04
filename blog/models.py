343d6258952c561007342f3ae24d1d0390c62bd5
Rajat Gupta <rajat.gupta712@gmail.com>
2015-05-04 15:40:35 +0530

    Added Boiler Plate

---
+++ blog/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+from django.contrib import admin
+
+
+class BlogPost(models.Model):
+    """
+    Model Structure of Blog in DB
+    """
+    title = models.CharField(max_length=150)
+    body = models.TextField()
+    timestamp = models.DateTimeField()
+
+    """
+    def __init__(self, title, body, timestamp):
+        self.title = title
+        self.body = body
+        self.timestamp = timestamp
+    """
+
+
+class BlogPostAdmin(admin.ModelAdmin):
+    """
+    Model for admin
+    """
+    list_display = ('title', 'timestamp')
+
+admin.site.register(BlogPost, BlogPostAdmin)
